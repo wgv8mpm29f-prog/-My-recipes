052f0f25bfecde1d7c6b28c68508c498bf01275f
wgv8mpm29f-prog <wgv8mpm29f@privaterelay.appleid.com>
2026-09-01 19:58:10 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='recipes-app-v3';
+const CACHE='recipes-app-v4';
 const CORE=['./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
